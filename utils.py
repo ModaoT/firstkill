8d5cd6fe7f2025ecc1e592442269255fce6c4d6f
赵磊 <bhneo@126.com>
2018-04-23 19:36:26 +0800

    合并部分维度以简化运算

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from collections import Counter
 
+import sys
+
 
 def create_lookup_tables(words):
     word_counts = Counter(words)
@@ -11,6 +13,8 @@ def create_lookup_tables(words):
 
 def remove_rare_labels(source, remove_list, fill_na='0'):
     cleaned = source.copy()
+    length = len(cleaned)
+    step = length // 100
     for i, e in enumerate(source.fillna(fill_na)):
         if e.isdigit():
             if int(e) in remove_list:
@@ -28,18 +32,31 @@ def remove_rare_labels(source, remove_list, fill_na='0'):
                 formed = '-'
                 formed = formed.join(elements)
                 cleaned[i] = formed
+        if i % step == 0:
+            sys.stdout.write('progress: ' + str(i // step) + '\r')
+            sys.stdout.flush()
+    sys.stdout.write('progress: 100\n')
+    sys.stdout.flush()
     return cleaned
 
 
 def count_element(feature_box, fill_na='0'):
     collector = list()
+    length = len(feature_box)
+    step = length // 100
+    filled_feature = feature_box.fillna(fill_na)
     for i, e in enumerate(feature_box.fillna(fill_na)):
+        e = filled_feature[i]
         if e.isdigit():
             collector.append(int(e))
         else:
             for ee in e.split('-'):
                 collector.append(int(ee))
-
+        if i % step == 0:
+            sys.stdout.write('progress: ' + str(i // step) + '\r')
+            sys.stdout.flush()
+    sys.stdout.write('progress: 100\n')
+    sys.stdout.flush()
     return collector
 
 
@@ -68,4 +85,53 @@ def asset_clean_result(cleaned_count, boundary, less, more):
     print(len(more))
     print(len(dic_less_than_cleaned))
     print(len(dic_more_than_cleaned))
+
+
+def add_base_for_all(feature_box, base):
+    added_feature = feature_box.copy()
+    length = len(added_feature)
+    step = length // 10
+    for i, e in enumerate(feature_box.fillna('NaN')):
+        if e == 'NaN':
+            continue
+        if e.isdigit():
+            added_feature[i] = str(int(e) + base)
+        else:
+            elements = list()
+            for ee in e.split('-'):
+                elements.append(str(int(ee) + base))
+            formed = '-'
+            formed = formed.join(elements)
+            added_feature[i] = formed
+        if i % step == 0:
+            sys.stdout.write('progress: ' + str(10 * i // step) + '%\r')
+            sys.stdout.flush()
+    sys.stdout.write('progress: 100%\n')
+    sys.stdout.flush()
+    return added_feature
+
+
+def merge_feature(source, feature1, feature2):
+    merged = source.copy()
+    f1 = source[feature1].copy()
+    f2 = source[feature2].fillna('NaN')
+    length = len(f1)
+    step = length // 10
+    for i, e in enumerate(f1.fillna('NaN')):
+        if e == 'NaN':
+            f1[i] = f2[i]
+        else:
+            if f2[i] != 'NaN':
+                formed = '-'
+                formed = formed.join([e, f2[i]])
+                f1[i] = formed
+        if i % step == 0:
+            sys.stdout.write('progress: ' + str(10 * i // step) + '%\r')
+            sys.stdout.flush()
+    sys.stdout.write('progress: 100%\n')
+    sys.stdout.flush()
+    merged[feature1] = f1
+    return merged.drop(feature2, axis=1).rename(columns={feature1: feature1+feature2})
+
+
 
